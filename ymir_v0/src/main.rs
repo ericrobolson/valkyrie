1974cd06f00430959daaf3050c7c703e7495a319
Eric Olson <eric.rob.olson@gmail.com>
2021-02-02 08:07:14 -0800

    Added in basic string handling.

--- ymir_v0/src/main.rs
+++ ymir_v0/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::io::Write;
 use std::{io, iter::Peekable};
 
@@ -44,6 +45,8 @@ pub fn eval(input: String) -> Result<String, String> {
 }
 
 const MUTABLE: &'static str = "mut";
+const TRUE: &'static str = "true";
+const FALSE: &'static str = "false";
 
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub enum Paren {
@@ -53,9 +56,11 @@ pub enum Paren {
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum Literal {
-    String(String),
+    /// The boolean type
     Bool(bool),
-    Number(String),
+    /// The integer type
+    I32(i32),
+    String(String),
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -71,12 +76,12 @@ pub enum Token {
     /// The mutable specifier 'mut'.
     Mutable,
     /// Identifier
+    /// TODO: finish this up by adding tests for invalid types
     Identifier(String),
 
     // TODO: implement
     Comment(String),
     Literal(Literal),
-    Type(Box<Token>),
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -86,11 +91,12 @@ pub struct LexErr {
     pub message: String,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum WorkingState {
     None,
     Symbol(String),
     Identifier(String),
+    String(String),
 }
 
 struct ParseResult {
@@ -114,52 +120,109 @@ fn parse_character(
     let mut terminated_symbol = is_final_token;
     let mut is_newline = false;
 
+    let add_to_string = |c: char, string_state: &WorkingState| -> Option<WorkingState> {
+        match string_state {
+            WorkingState::String(s) => {
+                let mut s = s.clone();
+                s.push(c);
+
+                Some(WorkingState::String(s))
+            }
+            _ => None,
+        }
+    };
+
     match token {
         '(' | ')' => {
-            token_to_add = match token {
-                '(' => Some(Token::LParen),
-                ')' => Some(Token::RParen),
-                _ => None,
-            };
-            terminated_symbol = true;
+            //
+            match add_to_string(token, state) {
+                Some(s) => {
+                    new_state = s;
+                }
+                None => {
+                    token_to_add = match token {
+                        '(' => Some(Token::LParen),
+                        ')' => Some(Token::RParen),
+                        _ => None,
+                    };
+                    terminated_symbol = true;
+                }
+            }
         }
         ' ' => {
-            terminated_symbol = true;
+            //
+            match add_to_string(token, state) {
+                Some(s) => {
+                    new_state = s;
+                }
+                None => {
+                    terminated_symbol = true;
+                }
+            }
         }
         '\r' | '\n' => {
-            is_newline = true;
-            terminated_symbol = true;
+            //
+            match add_to_string(token, state) {
+                Some(s) => {
+                    new_state = s;
+                }
+                None => {
+                    is_newline = true;
+                    terminated_symbol = true;
+                }
+            }
         }
         ':' => {
-            terminated_symbol = true;
-            token_to_add = Some(Token::Specifier);
+            //
+            match add_to_string(token, state) {
+                Some(s) => {
+                    new_state = s;
+                }
+                None => {
+                    terminated_symbol = true;
+                    token_to_add = Some(Token::Specifier);
+                }
+            }
         }
-        '\'' => match state {
-            WorkingState::Identifier(_) => {
-                let line_number = *line_number;
-                let character_number = *character_number;
+        '\'' => {
+            //
+            match add_to_string(token, state) {
+                Some(s) => {
+                    new_state = s;
+                }
+                None => match state {
+                    WorkingState::Identifier(_) => {
+                        let line_number = *line_number;
+                        let character_number = *character_number;
 
-                return Err(LexErr{
-                    line_number, character_number, message: "Found a symbol while parsing an identifier! `(test'symbol2')` should be `(test 'symbol2)`.".into()
-                });
-            }
-            WorkingState::None => {
-                new_state = WorkingState::Symbol(String::new());
-            }
-            WorkingState::Symbol(_) => {
-                let line_number = *line_number;
-                let character_number = *character_number;
+                        return Err(LexErr{
+                            line_number, character_number, message: "Found a symbol while parsing an identifier! `(test'symbol2')` should be `(test 'symbol2)`.".into()
+                        });
+                    }
+                    WorkingState::None => {
+                        new_state = WorkingState::Symbol(String::new());
+                    }
+                    WorkingState::Symbol(_) => {
+                        let line_number = *line_number;
+                        let character_number = *character_number;
 
-                return Err(LexErr{
-                    line_number, character_number, message: "Found a symbol while parsing a previous symbol! `('symbol-1'symbol2')` should be `('symbol1 'symbol2)`.".into()
-                });
+                        return Err(LexErr{
+                            line_number, character_number, message: "Found a symbol while parsing a previous symbol! `('symbol-1'symbol2')` should be `('symbol1 'symbol2)`.".into()
+                        });
+                    }
+                    WorkingState::String(_) => {}
+                },
             }
-        },
+        }
         _ => match state {
             WorkingState::None => {
-                let mut identifier = String::new();
-                identifier.push(token);
-                new_state = WorkingState::Identifier(identifier);
+                if token == '"' {
+                    new_state = WorkingState::String(String::new());
+                } else {
+                    let mut identifier = String::new();
+                    identifier.push(token);
+                    new_state = WorkingState::Identifier(identifier);
+                }
             }
             WorkingState::Symbol(symbol) => {
                 let mut symbol = symbol.clone();
@@ -170,6 +233,28 @@ fn parse_character(
                 let mut identifier = identifier.clone();
                 identifier.push(token);
                 new_state = WorkingState::Identifier(identifier);
+            }
+            WorkingState::String(string) => {
+                // Add character to string
+                match add_to_string(token, state) {
+                    Some(s) => {
+                        match s {
+                            WorkingState::String(s) => {
+                                // Only end string if it's not an escaped end quote
+                                if s.ends_with("\"") {
+                                    token_to_add =
+                                        Some(Token::Literal(Literal::String(string.clone())));
+                                    new_state = WorkingState::None;
+                                } else {
+                                    // set new state;
+                                    new_state = WorkingState::String(s);
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    None => {}
+                }
             }
         },
     }
@@ -186,24 +271,35 @@ fn parse_character(
         }
         WorkingState::Identifier(identifier) => {
             if terminated_symbol {
-                if identifier.as_str() == MUTABLE {
-                    added_tokens.push(Token::Mutable);
-                } else if identifier.to_lowercase().as_str() == MUTABLE {
-                    let line_number = *line_number;
-                    let character_number = *character_number;
+                let lowercase = identifier.to_lowercase();
 
-                    return Err(LexErr {
-                        line_number,
-                        character_number,
-                        message:
-                            "Attempted to use '{:?}' as an identifier! 'mut' is a reserved keyword."
-                                .into(),
-                    });
+                let int_result = lowercase.parse::<i32>();
+                if int_result.is_ok() {
+                    let result = int_result.unwrap();
+                    added_tokens.push(Token::Literal(Literal::I32(result)));
+                } else if lowercase.as_str() == MUTABLE {
+                    added_tokens.push(Token::Mutable);
+                } else if lowercase.as_str() == TRUE {
+                    added_tokens.push(Token::Literal(Literal::Bool(true)));
+                } else if lowercase.as_str() == FALSE {
+                    added_tokens.push(Token::Literal(Literal::Bool(false)));
                 } else {
                     added_tokens.push(Token::Identifier(identifier.clone()));
                 }
 
                 WorkingState::None
+            } else {
+                new_state
+            }
+        }
+        WorkingState::String(s) => {
+            if is_final_token {
+                let line_number = *line_number;
+                let character_number = *character_number;
+
+                return Err(LexErr{
+                    line_number, character_number, message: "Found an string that wasn\'t closed! `\"test string` should be `\"test string\"`.".into()
+                });
             } else {
                 new_state
             }
@@ -236,6 +332,7 @@ pub fn lex(program: String) -> Result<Vec<Token>, LexErr> {
     let mut line_number = 0;
     let mut character_number = 0;
     let mut state = WorkingState::None;
+
     let program = program.chars().collect::<Vec<char>>();
 
     for (index, token) in program.iter().enumerate() {
@@ -262,8 +359,92 @@ mod tests {
     use super::*;
 
     #[test]
-    fn lex_identifier_not_a_literal() {
-        todo!("Test identifiers start with anything that isn't a string, a number, a bool, etc.");
+    fn lex_string_returns_ok() {
+        let (s, expected) = (
+            "\"-2432:\n\n`'\"",
+            vec![Token::Literal(Literal::String("-2432:\n\n`'".into()))],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn lex_i32_returns_ok() {
+        let (s, expected) = (
+            "-2432:",
+            vec![Token::Literal(Literal::I32(-2432)), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+
+        let (s, expected) = (
+            "9999932:",
+            vec![Token::Literal(Literal::I32(9999932)), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+
+        let (s, expected) = (
+            "3.14:",
+            vec![Token::Identifier("3.14".into()), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn lex_bool_returns_ok() {
+        let (s, expected) = (
+            "true:",
+            vec![Token::Literal(Literal::Bool(true)), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+
+        let (s, expected) = (
+            "tRuE:\n",
+            vec![Token::Literal(Literal::Bool(true)), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+
+        let (s, expected) = (
+            "false:\n",
+            vec![Token::Literal(Literal::Bool(false)), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+
+        let (s, expected) = (
+            "fALSe:\n",
+            vec![Token::Literal(Literal::Bool(false)), Token::Specifier],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
     }
 
     #[test]
@@ -277,6 +458,21 @@ mod tests {
 
         let (s, expected) = (
             "(mut:)",
+            vec![
+                Token::LParen,
+                Token::Mutable,
+                Token::Specifier,
+                Token::RParen,
+            ],
+        );
+        let result = lex(s.into());
+        assert_eq!(true, result.is_ok());
+
+        let actual = result.unwrap();
+        assert_eq!(expected, actual);
+
+        let (s, expected) = (
+            "(MuT:)",
             vec![
                 Token::LParen,
                 Token::Mutable,
