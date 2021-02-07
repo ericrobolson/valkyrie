d7cdb5c6360c13eccbe4089abbbf274e39bb4711
Eric Olson <eric.rob.olson@gmail.com>
2021-02-06 18:19:29 -0800

    Added ios targets.

--- install_targets.bat
+++ install_targets.bat
@@ -1,10 +1,24 @@
+REM ANDROID
 rustup target add arm-linux-androideabi
 rustup target add armv7-linux-androideabi
 rustup target add aarch64-linux-android
 rustup target add i686-linux-android
 rustup target add x86_64-linux-android
-rustup target add x86_64-pc-windows-msvc
-rustup target add wasm32-unknown-unknown
-
 cargo install cargo-apk
+
+REM IOS
+rustup target add aarch64-apple-ios 
+rustup target add aarch64-apple-ios 
+rustup target add x86_64-apple-ios 
+xcode-select --install
+cargo install cargo-lipo
+
+REM Windows
+rustup target add x86_64-pc-windows-msvc
+
+REM WASM
+rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
+   
+
+set /p Input=Press any key to exit
