06fccbad3b07a4e04f354bb3e952f585921667f1
Eric Olson <eric.rob.olson@gmail.com>
2021-02-06 20:31:04 -0800

    Revert "Added ios targets."
    This reverts commit d7cdb5c6360c13eccbe4089abbbf274e39bb4711.

--- install_targets.bat
+++ install_targets.bat
@@ -1,24 +1,10 @@
-REM ANDROID
 rustup target add arm-linux-androideabi
 rustup target add armv7-linux-androideabi
 rustup target add aarch64-linux-android
 rustup target add i686-linux-android
 rustup target add x86_64-linux-android
-cargo install cargo-apk
-
-REM IOS
-rustup target add aarch64-apple-ios 
-rustup target add aarch64-apple-ios 
-rustup target add x86_64-apple-ios 
-xcode-select --install
-cargo install cargo-lipo
-
-REM Windows
 rustup target add x86_64-pc-windows-msvc
-
-REM WASM
 rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli
-   
 
-set /p Input=Press any key to exit
+cargo install cargo-apk
+cargo install wasm-bindgen-cli
--- verify.bat
+++ verify.bat
@@ -7,15 +7,8 @@ cargo build --target=x86_64-pc-windows-msvc
 echo !! BEGIN ANDROID BUILDS !!
 cargo apk build
 
-echo !! BEGIN IOS BUILDS !!
-
-cargo build --target=aarch64-apple-ios 
-cargo build --target=aarch64-apple-ios 
-cargo build --target=x86_64-apple-ios 
-
-
-echo !! BEGIN WEB BUILDS TODO !!
-###cargo build --target wasm32-unknown-unknown
+echo !! BEGIN WEB BUILDS - done last due to flags !!
+cargo build --target wasm32-unknown-unknown
 
 echo !! !! !! !!
 set /p Input=Press any key to exit
