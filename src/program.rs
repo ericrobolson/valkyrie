c69a83a6b5e6745d3bbf1ba60afb141ce6f392d4
Eric Olson <eric.rob.olson@gmail.com>
2021-02-06 20:31:09 -0800

    Revert "Started doing multi-target build."
    This reverts commit 5e5c170343b5accbdfbf3e6174cb7fbae675a71e.

--- src/program.rs
+++
@@ -1,3 +0,0 @@
-pub fn program() -> Result<(), String> {
-    Ok(())
-}
--- verify.bat
+++ verify.bat
@@ -1,16 +1,5 @@
-echo !! BEGIN NATIVE BUILD !!
 cargo build
-
-echo !! BEGIN WINDOWS BUILDS !!
 cargo build --target=x86_64-pc-windows-msvc		
-
-echo !! BEGIN ANDROID BUILDS !!
 cargo apk build
 
-echo !! BEGIN WEB BUILDS - done last due to flags !!
-cargo build --target wasm32-unknown-unknown
-
-echo !! !! !! !!
 set /p Input=Press any key to exit
-
-
